122e8db1986a2217866e2b9606dee2c026c283d0
ydah <t.yudai92@gmail.com>
2025-04-02 23:31:39 +0900

    Remove unnecessary module definitions from rubocop-rspec-extra.rb

--- lib/rubocop-rspec-extra.rb
+++ lib/rubocop-rspec-extra.rb
@@ -8,11 +8,3 @@ require_relative "rubocop/cop/rspec/extra/be_empty"
 require_relative "rubocop/cop/rspec/extra/flat_examples"
 require_relative "rubocop/cop/rspec/extra/restrict_block_tag"
 require_relative "rubocop/cop/rspec/extra/restrict_block_tag_value"
-
-module Rubocop
-  module RSpec
-    # RuboCop RSpec Rails project namespace.
-    module Extra
-    end
-  end
-end
